b6ea8184a6eb9abf3c20b6d711678053ea7fb69d
semantic-release <semantic-release>
2025-12-01 00:35:12 +0000

    chore(edt-release): release extended-data-types v202511.7.1 [skip ci]
    Automatically generated by python-semantic-release

--- packages/extended-data-types/src/extended_data_types/__init__.py
+++ packages/extended-data-types/src/extended_data_types/__init__.py
@@ -105,7 +105,7 @@ from .type_utils import (
 from .yaml_utils import decode_yaml, encode_yaml, is_yaml_data
 
 
-__version__ = "202511.7.0"
+__version__ = "202511.7.1"
 
 __all__ = [
     "FilePath",
